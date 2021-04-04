f26cf6a1622517cff97d5fa8ffe56a9b5f18ce6e
Edward Gold Edem Hogan <edge3769@gmail.com>
2021-04-04 13:51:30 -0400

    removed UBA account number causing

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -75,7 +75,6 @@
       api.post('subs', {id: $session.user.id, sub: sub})
     })
   }  
-2057577094 - UBA
   // if(typeof window != 'undefined'){
   //   if(navigator && navigator.serviceWorker && $session.user){
   //     getSub()  

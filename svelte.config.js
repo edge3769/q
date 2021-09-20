d4e01760f5910c38764562ac98b91f9fa12c45cb
EdwardGold Edem Hogan <edge37@outlook.com>
2021-09-20 02:48:02 +0100

    cssHash, dependencies upgrade

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,11 @@
 import adapter from '@sveltejs/adapter-static'
 
 export default {
+	compilerOptions: {
+		cssHash: (hash, css, name, filename) => {
+			return `${filename}-${name}`
+		}
+	},
 	kit: {
 		adapter: adapter({
 			pages: 'build',
